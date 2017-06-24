e5dbd17ca9e10b952b5dc6eccad2c2a7441a9af5
k8tems <maamalddz@hotmail.com>
2017-06-25 00:39:33 +0100

    Separate tests

--- test/unit/reducers/tweets.js
+++ test/unit/reducers/tweets.js
@@ -2,13 +2,16 @@ import assert from 'power-assert';
 import handleActions from '../../../src/renderer/src/reducers/tweets';
 
 describe('Tweets test', () => {
+  const oldState = { columns: [{ id: '1', isConversationOpened: false }] };
+  const action = { payload: { timelineId: '1' }, type: 'OPEN_CONVERSATION'};
+  const newState = handleActions(oldState, action);
+
   it('`isConversationOpened` should be set to true', () => {
-    const oldState = { columns: [{ id: '1', isConversationOpened: false }] };
-    const action = { payload: { timelineId: '1' }, type: 'OPEN_CONVERSATION'};
-    const newState = handleActions(oldState, action);
     assert.equal(false, oldState.columns[0].isConversationOpened);
     assert.equal(true, newState.columns[0].isConversationOpened);
-    // make sure other properties are preserved
+  });
+
+  it('Other properties are preserved', () => {
     assert.equal('1', oldState.columns[0].id);
     assert.equal('1', newState.columns[0].id);
   });
