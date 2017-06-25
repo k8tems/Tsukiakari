5da5b818444c29317d45452f33dbd4d767d9bdfb
k8tems <maamalddz@hotmail.com>
2017-06-25 01:07:58 +0100

    Add additional column

--- test/unit/reducers/tweets.js
+++ test/unit/reducers/tweets.js
@@ -2,17 +2,26 @@ import assert from 'power-assert';
 import handleActions from '../../../src/renderer/src/reducers/tweets';
 
 describe('Tweets test', () => {
-  const oldState = { columns: [{ id: '1', isConversationOpened: false }] };
+  const oldState = { columns: [
+    { id: '1', isConversationOpened: false, ex: 'ex' },
+    { id: '2', isConversationOpened: false, ex: 'ex' },] };
   const action = { payload: { timelineId: '1' }, type: 'OPEN_CONVERSATION'};
   const newState = handleActions(oldState, action);
 
+  function getColumnById(columns, id) {
+    return columns.filter(c => c.id === id)[0];
+  }
+
   it('`isConversationOpened` should be set to true', () => {
-    assert.equal(false, oldState.columns[0].isConversationOpened);
-    assert.equal(true, newState.columns[0].isConversationOpened);
+    assert.equal(false, getColumnById(oldState.columns, '1').isConversationOpened);
+    assert.equal(true, getColumnById(newState.columns, '1').isConversationOpened);
   });
 
   it('Other properties are preserved', () => {
-    assert.equal('1', oldState.columns[0].id);
-    assert.equal('1', newState.columns[0].id);
+    assert.equal('ex', getColumnById(oldState.columns, '1').ex);
+    assert.equal('ex', getColumnById(newState.columns, '1').ex);
+  });
+
+  it('Column order is preserved', () => {
   });
 });
